34da4464a0e452ca6b2f92ceed3319816c683a91
PauloHBSF <paulohbsfilho@gmail.com>
2025-07-15 23:31:28 -0300

    :sparkles: implementado nome do usuário no headër

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -4,15 +4,21 @@ import logoutSvg from "../assets/logout.svg";
 import { useAuth } from "../hooks/useAuth";
 
 export function Header() {
-
-    const auth = useAuth()
+    const auth = useAuth();
 
     return (
         <header className="w-full flex justify-between my-8">
             <img src={logoSvg} alt="Logo" />
             <div className="flex items-center gap-3">
-                <span className="text-sm font-semibold text-gray-200">Olá [username]</span>
-                <img src={logoutSvg} alt="Ícone de Saír" className="my-8 cursor-pointer hover:opacity-85 transition ease-linear" onClick={() => auth.remove()}/>
+                <span className="text-sm font-semibold text-gray-200">
+                    Olá, {auth.session?.user.name}
+                </span>
+                <img
+                    src={logoutSvg}
+                    alt="Ícone de Saír"
+                    className="my-8 cursor-pointer hover:opacity-85 transition ease-linear"
+                    onClick={() => auth.remove()}
+                />
             </div>
         </header>
     );
